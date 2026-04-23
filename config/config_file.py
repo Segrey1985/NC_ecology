b70929119cffa20e85320ddd0c86701fa63cec84
maxfil333NC <maxfil333@gmail.com>
2026-04-23 12:09:00 +0300

    feat(project_data): add embeddings

--- config/config_file.py
+++ config/config_file.py
@@ -1,10 +1,20 @@
+import os
 import pathlib
-from dataclasses import dataclass
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 
 
-@dataclass
-class Config:
-    BASE_DIR: str = pathlib.Path(__file__).resolve().parent.parent
+class Config(BaseSettings):
+    HF_TOKEN: str
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+    BASE_DIR: Path = BASE_DIR
 
 
 cfg = Config()
---
+++ src/project_data/embeddings.py
@@ -0,0 +1,25 @@
+import os
+from sentence_transformers import SentenceTransformer
+
+from config.config_file import cfg
+from src.utils.logger import logger
+
+
+def load_embeddings(model_name: str):
+    models_dir = cfg.BASE_DIR / "data" / "__local_models"
+    model_name_str = model_name.replace("/", "_")
+    model_path = models_dir / model_name_str
+    if os.path.exists(model_path):
+        logger.debug(f"Loading local model from {model_path}...")
+        embedder = SentenceTransformer(model_path.as_posix())
+    else:
+        logger.debug(f"Loading model from HuggingFace...")
+        embedder = SentenceTransformer(model_name)
+        embedder.save(model_path)
+    logger.debug(f"Loading model is finished.")
+    return embedder
+
+
+if __name__ == "__main__":
+    model_name = "Qwen/Qwen3-Embedding-8B"
+    embedder = load_embeddings(model_name)
