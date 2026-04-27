49650b8e681c6cca59da2fb10e92253409815122
maxfil333NC <maxfil333@gmail.com>
2026-04-27 09:57:00 +0300

    feat(core): add openai embeddings

--- config/config_file.py
+++ config/config_file.py
@@ -1,4 +1,3 @@
-import os
 import pathlib
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -8,6 +7,7 @@ BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 
 class Config(BaseSettings):
     HF_TOKEN: str
+    AI_TUNNEL_API_KEY: str
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env",
         env_file_encoding="utf-8",
--- src/project_data/embeddings.py
+++ src/project_data/embeddings.py
@@ -1,11 +1,12 @@
 import os
+from openai import OpenAI
 from sentence_transformers import SentenceTransformer
 
 from config.config_file import cfg
 from src.utils.logger import logger
 
 
-def load_embeddings(model_name: str):
+def load_local_embedder(model_name: str):
     models_dir = cfg.BASE_DIR / "data" / "__local_models"
     model_name_str = model_name.replace("/", "_")
     model_path = models_dir / model_name_str
@@ -16,10 +17,68 @@ def load_embeddings(model_name: str):
         logger.debug(f"Loading model from HuggingFace...")
         embedder = SentenceTransformer(model_name, device="cuda")
         embedder.save(model_path)
-    logger.debug(f"Loading model is finished.")
+    logger.debug(f"Model loading complete.")
     return embedder
 
 
+def load_openai_embeddings_client() -> OpenAI:
+    client = OpenAI(
+        api_key=cfg.AI_TUNNEL_API_KEY,
+        base_url="https://api.aitunnel.ru/v1/"
+    )
+    
+    return client
+    
+
+class OpenAIEmbedder:
+    def __init__(self, model="text-embedding-3-small"):
+        self.client = load_openai_embeddings_client()
+        self.model = model
+
+    def encode(self, texts, batch_size=32):
+        if isinstance(texts, str):
+            texts = [texts]
+
+        all_embeddings = []
+        for i in range(0, len(texts), batch_size):
+            batch = texts[i:i+batch_size]
+            response = self.client.embeddings.create(
+                model=self.model,
+                input=batch
+            )
+            all_embeddings.extend([x.embedding for x in response.data])
+
+        return all_embeddings
+    
+
+def init_openai_embedder():
+    return OpenAIEmbedder()
+
+
 if __name__ == "__main__":
+    
     model_name = "Qwen/Qwen3-Embedding-8B"
-    embedder = load_embeddings(model_name)
+    embedder = load_local_embedder(model_name)
+    vectors = embedder.encode(['раз', 'два'], batch_size=2)
+    print(vectors)
+    
+    openai_embedder = init_openai_embedder()
+    vectors = openai_embedder.encode(['раз', 'два'])
+    print(vectors)
+    
+    sentences = [
+        "The weather is lovely today.",
+        "It's so sunny outside!",
+        "He drove to the stadium."
+    ]
+    
+    model_name = "Qwen/Qwen3-Embedding-8B"
+    embedder = load_local_embedder(model_name)
+    embeddings = embedder.encode(sentences, batch_size=32)
+    similarities = embedder.similarity(embeddings, embeddings)
+    print(similarities.shape)
+    
+    openai_embedder = init_openai_embedder()
+    embeddings = openai_embedder.encode(sentences, batch_size=32)
+    print(type(embeddings))
+    print(embeddings)
