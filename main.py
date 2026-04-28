ca8f09c3e7a143fcf2ff5220e30b5988af1615d3
maxfil333NC <maxfil333@gmail.com>
2026-04-28 16:53:50 +0300

    refactor main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import uuid
+from pprint import pprint
 from pathlib import Path
 from langchain_core.messages import HumanMessage
 
@@ -9,9 +10,54 @@ from src.utils.utils import print_chunk
 
 __placeholders_example = {
     "НАИМЕНОВАНИЕ_ПРОЕКТА": "Наименование проекта",
-    "ТИП_РАБОТ": " тип строительных работ для склонения в тексте (например, 'строительства', 'реконструкции', 'технического перевооружения')."
+    "ТИП_РАБОТ": " тип строительных работ для склонения в тексте (например, 'строительства', 'реконструкции', 'технического перевооружения').",
 }
-    
+
+
+def _load_placeholders(placeholders_path: Path, table_placeholders_path: Path | None):
+
+    if table_placeholders_path:
+        with open(
+            table_placeholders_path, "r", encoding="utf-8"
+        ) as table_placeholders_file:
+            table_placeholders = json.load(table_placeholders_file)
+    else:
+        table_placeholders = {}
+
+    with open(placeholders_path, "r", encoding="utf-8") as placeholders_file:
+        placeholders = json.load(placeholders_file)
+
+    placeholders = {
+        k: v for k, v in placeholders.items() if k not in table_placeholders
+    }
+    return placeholders, table_placeholders
+
+
+def _run_graph(detailed_placeholder, verbose=True) -> str:
+
+    config = {"configurable": {"thread_id": str(uuid.uuid4())}}
+    config.update(langfuse_config)
+
+    input_messages = [HumanMessage(detailed_placeholder)]
+
+    final_content = ""
+    for chunk in graph.stream(
+        input={
+            "messages": input_messages,
+            "input_query": input_messages[0].content,
+        },
+        stream_mode="updates",
+        config=config,
+    ):
+        if verbose:
+            print_chunk(chunk)
+
+        if "structured_output_node" in chunk:
+            final_output = chunk["structured_output_node"]
+            final_content = final_output["messages"][-1].content
+
+    return final_content
+
 
 def main(
     template_docx_path: Path | None,
@@ -19,55 +65,28 @@ def main(
     table_placeholders_path: Path | None,
     project_parts_path: Path | None,
 ):
-    
     output = {}
-    
-    if table_placeholders_path:
-        with open(table_placeholders_path, "r", encoding="utf-8") as table_placeholders_file:
-            table_placeholders = json.load(table_placeholders_file)
-    else:
-        table_placeholders = {}
-        
-    with open(placeholders_path, "r", encoding="utf-8") as placeholders_file:
-        placeholders = json.load(placeholders_file)
-        
-    placeholders = {k: v for k, v in placeholders.items() if k not in table_placeholders}
-    
-    final_content = ""
+    placeholders, table_placeholders = _load_placeholders(
+        placeholders_path, table_placeholders_path
+    )
     for placeholder, detailed_placeholder in placeholders.items():
-        
-        config = {"configurable": {"thread_id": str(uuid.uuid4())}}
-        config.update(langfuse_config)
-        
-        input_messages = [HumanMessage(detailed_placeholder)]
-        
-        for chunk in graph.stream(
-            input={
-                "messages": input_messages,
-                "input_query": input_messages[0].content,
-            },
-            stream_mode="updates",
-            config=config
-        ):
-            print_chunk(chunk)
-            
-            if "structured_output_node" in chunk:
-                final_output = chunk["structured_output_node"]
-                final_content = final_output["messages"][-1].content
-        
+        final_content = _run_graph(detailed_placeholder, verbose=True)
         output[placeholder] = json.loads(final_content).get("answer", "__empty__")
-        # break
-    
+        break
     return output
 
+
 if __name__ == "__main__":
-    
+
     project_path = Path(__file__).parent
     output = main(
-        template_docx_path = None,
-        placeholders_path = project_path / "data" / "IN" / "templates" / "Анализ_и_введение.json" ,
-        table_placeholders_path = None,
-        project_parts_path = None,
+        template_docx_path=None,
+        placeholders_path=project_path
+        / "data"
+        / "IN"
+        / "templates"
+        / "Анализ_и_введение.json",
+        table_placeholders_path=None,
+        project_parts_path=None,
     )
-    
-    print(output)
+    pprint(output)
