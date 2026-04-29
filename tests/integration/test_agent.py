6db6ba0b03586947e3947185468728b868183a7e
maxfil333NC <maxfil333@gmail.com>
2026-04-29 15:59:55 +0300

    feat(tests): add integration tests

---
+++ tests/integration/test_agent.py
@@ -0,0 +1,20 @@
+import uuid
+from langchain_core.messages import HumanMessage
+
+from config.langfuse_client import langfuse_config
+from agent import build_graph
+
+def test_agent():
+    graph = build_graph()
+    config = {"configurable": {"thread_id": str(uuid.uuid4())}}
+    config.update(langfuse_config)
+    input_messages = [HumanMessage('Проектируемые электросети')]
+    for chunk in graph.stream(
+        input={
+            "messages": input_messages,
+            "input_query": input_messages[0].content,
+        },
+        stream_mode="updates",
+        config=config
+    ):
+        print(chunk)
---
+++ tests/integration/test_main.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import tempfile
+
+from main import main
+
+def test_main():
+    base = Path(__file__).parents[2]
+    with tempfile.TemporaryDirectory(prefix="nc_ecology_test_") as tmp_dir:
+        output_dir = Path(tmp_dir) / "project1"
+        main(
+            template_docx_path=base / "data" / "IN" / "templates" / "Анализ_и_введение.docx",
+            placeholders_path=base / "data" / "IN" / "templates" / "Анализ_и_введение.json",
+            table_placeholders_path=None,
+            project_parts_path=None,
+            output_path=output_dir,
+            collection_name="main",
+        )
+
+        created_files = [p for p in output_dir.rglob("*") if p.is_file()]
+        assert len(created_files) == 2, f"Ожидалось 2 файла, найдено {len(created_files)}: {created_files}"
