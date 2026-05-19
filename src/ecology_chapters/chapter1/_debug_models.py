6e962bc533ba6f7933b2bf9a4d88e7c17791f2c9
maxfil333NC <maxfil333@gmail.com>
2026-05-19 15:44:41 +0300

    update template

--- src/ecology_chapters/chapter1/_debug_models.py
+++ src/ecology_chapters/chapter1/_debug_models.py
@@ -11,5 +11,5 @@ PowerSupply, WaterTreatment, UtilityNetworks, Ventilation
 
 # ACTIVE_MODEL_NAMES: list[str] | None = None
 
-ACTIVE_MODEL_NAMES = ["Architecture", "Boilers", "NearestObjects", "Surroundings", "GeneralPlan"]
-# ACTIVE_MODEL_NAMES = ["Architecture"]
+# ACTIVE_MODEL_NAMES = ["Architecture", "Boilers", "NearestObjects", "Surroundings", "GeneralPlan"]
+ACTIVE_MODEL_NAMES = ["Architecture"]
