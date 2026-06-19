6a8c3b1de60af1ead757f07a9115fab5c0508fb1
maxfil333NC <maxfil333@gmail.com>
2026-06-19 11:24:00 +0300

    fix(core): calculations bug fix

--- src/ecology_chapters/chapter0/calculations.py
+++ src/ecology_chapters/chapter0/calculations.py
@@ -11,7 +11,10 @@ def project_name_lower(placeholders: dict) -> None:
     key = "НАИМЕНОВАНИЕ_ПРОЕКТА"
     new_key = "НАИМЕНОВАНИЕ_ПРОЕКТА_lower"
     value = placeholders[key]
-    placeholders[new_key] = value[0].lower() + value[1:]
+    if not value:
+        placeholders[new_key] = "{{" + str(new_key) + "}}"
+        return
+    placeholders[new_key] = str(value)[0].lower() + str(value)[1:]
 
 
 def project_name_no_capacity(placeholders: dict) -> None:
@@ -19,8 +22,11 @@ def project_name_no_capacity(placeholders: dict) -> None:
     key = "НАИМЕНОВАНИЕ_ПРОЕКТА"
     new_key = "НАИМЕНОВАНИЕ_ПРОЕКТА_no_capacity"
     value = placeholders[key]
+    if not value:
+        placeholders[new_key] = "{{" + str(new_key) + "}}"
+        return
     regex = r"мощностью\s+\d+(?:[.,]\d+)?\s+мвт"
-    new_ = re.sub(regex, "", value, flags = re.IGNORECASE)
+    new_ = re.sub(regex, "", str(value), flags = re.IGNORECASE)
     new = re.sub(r"\s{2,}", " ", new_).strip()
     placeholders[new_key] = new[0].lower() + new[1:]
     
@@ -29,4 +35,7 @@ def project_name_genitive_lower(placeholders: dict) -> None:
     key = "name_genitive"
     new_key = "name_genitive_lower"
     value = placeholders[key]
-    placeholders[new_key] = value[0].lower() + value[1:]
+    if not value:
+        placeholders[new_key] = "{{" + str(new_key) + "}}"
+        return
+    placeholders[new_key] = str(value)[0].lower() + str(value)[1:]
